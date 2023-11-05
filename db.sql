4e2139091ee4b992f85cf7b37646511a5c682fbe
Ahmed Mohamed Abdelgwad <47200062+ahmed-m-abdelgawad@users.noreply.github.com>
2023-11-05 16:36:27 +0200

    Update db.sql

--- db.sql
+++ db.sql
@@ -122,3 +122,195 @@ function checkAllDays() {
 }
 
 </script> </head> <body> <h1>نموذج تسجيل بيانات العملاء</h1> <form method=“post” action=“<?php echo $_SERVER[‘PHP_SELF’]; ?>”> <p>اسم المستخدم: <?php echo $user_name; ?></p> <p>أسم العميل: <input type=“text” name=“customer_name” maxlength=“40” required></p> <p>العنوان: <input type=“text” name=“address” required></p> <p>رقم التليفون: <input type=“tel” name=“phone_number” pattern=“[0-9]{11}” required></p> <p>نوع المنفذ: <select name=“port_type” required> <option value=“”>اختر نوع المنفذ</option> <option value=“Ethernet”>Ethernet</option> <option value=“USB”>USB</option> <option value=“Wireless”>Wireless</option> </select></p> <p>ايام الزيارات: <input type=“checkbox” name=“visit_days[]” value=“السبت”> السبت <input type=“checkbox” name=“visit_days[]” value=“الاحد”> الاحد <input type=“checkbox” name=“visit_days[]” value=“الاثنين”> الاثنين <input type=“checkbox” name=“visit_days[]” value=“الثلاثاء”> الثلاثاء <input type=“checkbox” name=“visit_days[]” value=“الاربعاء”> الاربعاء <input type=“checkbox” name=“visit_days[]” value=“الخميس”> الخميس <input type=“checkbox” id=“all_days” value=“كل ايام الاسبوع” onclick=“checkAllDays()”> كل ايام الاسبوع </p> <p>تصنيف العميل: <select id=“customer_category” name=“customer_category” onchange=“showRoute()” required> <option value=“”>اختر تصنيف العميل</option> <option value=“Restaurant”>Restaurant</option> <option value=“Hotel”>Hotel</option> <option value=“Catering”>Catering</option> </select></p> <p>route: <select id=“route” name="route
+
+
+
+مساعدك القائم على الذكاء الاصطناعي للويب
+معاينة
+يتم تشغيل Bing بواسطة الذكاء الاصطناعي، لذا يمكن حدوث المفاجآت والأخطاء. الرجاء مشاركة الملاحظات حتى نتمكن من التحسين! 
+الشروط
+الخصوصية
+إليك بعض الأفكار لكي تبدأ:
+
+قم بتأليف دعوة لحفل استقبال المولود تتضمن أفكارًا للهدايا
+
+
+ما هي سلسلة فنادق اقتصادية جيدة تحتوي على مسبح؟
+
+
+ما هي الميزات الجديدة في أحدث إصدار من CSS؟
+
+مرحبًا، لقد عدت! سررت برؤيتك مجددا. ما الذي يمكنني مساعدتك في اليوم؟
+
+
+
+بأستخدام 
+Php , mysql , css , js , html5
+انشئ لى form
+لتسجيل بيانات العملاء والحقول كالتالى
+اسم المستخدم وهو عبارة عن عرض لأسم المستخدم الذى قام بتسجيل الدخول
+أسم العميل وهو بحد اقصي 40 حرف
+العنوان
+رقم التليفون ويجب ان يكون طوله 11 رقم
+نوع المنفذ
+ايام الزيارات وهو حقل من نوع check box لتحديد ايام الاسبوع وهم 
+السبت ، الاحد ، الاثنين ، الثلاثاء ، الاربعاء ، الخميس  ، كل ايام الاسبوع
+فى حالة اختيار كل ايام الاسبوع يتم اختيار جميع الايام من السبت الي الخميس
+حقل تصنيف العميل ينقسم الى
+Restaurant
+Hotel
+Catering
+حقل route وهو من نوع check box وتظهر الاختيارات به حسب اختيار تصنيف العميل 
+فى حالة اختيار restaurant تظهر 
+HO0007 , HO0008 , HO0009 , HO0010 , HO0016
+فى حالة اختيار Hotel تظهر 
+HO0005 , HO0006 , HO0013
+فى حالة اختيار catering تظهر 
+HO0001 , HO002 , HO0003 , HO0004 , HO0011 , HO0012 , HO0014
+زر ارسال لأرسال البيانات الي قاعدة البيانات MySQL 
+يجب ان يتم تسجيل الدخول بالاميل والباسورد وكل مستخدم يكون له صلاحية ظهور البيانات والتعديل بناء علي تصنيف القطاع التابع له المستخدم مع تصنيف العميل سواء 
+Hotel او restaurant  او catering
+مرحبا ، هذا بينغ. يمكنني مساعدتك في إنشاء نموذج لتسجيل بيانات العملاء باستخدام PHP ، MySQL ، CSS ، JS ، HTML5. هذا هو الكود الذي أنشأته لك:
+
+<code> <?php // اتصال بقاعدة البيانات MySQL $servername = “localhost”; $username = “root”; $password = “”; $dbname = “customers”;
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
+
+// تحقق من طلب النموذج if ($_SERVER[“REQUEST_METHOD”] == “POST”) { // جمع بيانات النموذج $user_name = $_POST[“user_name”]; $customer_name = $_POST[“customer_name”]; $address = $_POST[“address”]; $phone_number = $_POST[“phone_number”]; $port_type = $_POST[“port_type”]; $visit_days = $_POST[“visit_days”]; $customer_type = $_POST[“customer_type”]; $route = $_POST[“route”];
+
+// تحقق من صحة البيانات if (empty($user_name) || empty($customer_name) || empty($address) || empty($phone_number) || empty($port_type) || empty($visit_days) || empty($customer_type) || empty($route)) { echo “يرجى ملء جميع الحقول المطلوبة.”; } else if (strlen($customer_name) > 40) { echo “يجب ألا يزيد اسم العميل عن 40 حرفًا.”; } else if (strlen($phone_number) != 11) { echo “يجب أن يكون رقم الهاتف 11 رقمًا.”; } else { // إدخال البيانات في قاعدة البيانات $sql = “INSERT INTO customers (user_name, customer_name, address, phone_number, port_type, visit_days, customer_type, route) VALUES (‘$user_name’, ‘$customer_name’, ‘$address’, ‘$phone_number’, ‘$port_type’, ‘$visit_days’, ‘$customer_type’, ‘$route’)”;
+
+if ($conn->query($sql) === TRUE) {
+  echo "تم تسجيل بيانات العميل بنجاح.";
+} else {
+  echo "حدث خطأ أثناء تسجيل بيانات العميل: " . $conn->error;
+}
+
+} }
+
+// تحقق من تسجيل الدخول session_start(); if (!isset($_SESSION[“email”]) || !isset($_SESSION[“password”])) { header(“Location: login.php”); exit(); }
+
+// جلب تصنيف القطاع للمستخدم $user_sector = $_SESSION[“sector”];
+
+?>
+
+<!DOCTYPE html> <html lang=“ar”> <head> <meta charset=“UTF-8”> <title>نموذج تسجيل بيانات العملاء</title> <style> /* أنشئ نمط CSS للنموذج */ form { width: 600px; margin: 0 auto; border: 1px solid black; padding: 20px; font-family: Arial, sans-serif; }
+
+label {
+  display: block;
+  margin-bottom: 10px;
+}
+
+input, select {
+  width: 100%;
+  height: 30px;
+  font-size: 16px;
+}
+
+input[type="checkbox"] {
+  width: 20px;
+  height: 20px;
+}
+
+input[type="submit"] {
+  width: 150px;
+  margin-top: 20px;
+  background-color: green;
+  color: white;
+  border: none;
+}
+
+</style> <script> // أنشئ كود JS للنموذج // دالة لإظهار حقل route بناء على اختيار customer_type function showRoute() { var customer_type = document.getElementById(“customer_type”).value; var route = document.getElementById(“route”); var options = route.getElementsByTagName(“option”); // إخفاء جميع الخيارات for (var i = 0; i < options.length; i++) { options[i].style.display = “none”; } // إظهار الخيارات المناسبة if (customer_type == “Restaurant”) { options[0].style.display = “block”; options[1].style.display = “block”; options[2].style.display = “block”; options[3].style.display = “block”; options[4].style.display = “block”; } else if (customer_type == “Hotel”) { options[5].style.display = “block”; options[6].style.display = “block”; options[7].style.display = “block”; } else if (customer_type == “Catering”) { options[8].style.display = “block”; options[9].style.display = “block”; options[10].style.display = “block”; options[11].style.display = “block”; options[12].style.display = “block”; options[13].style.display = “block”; options[14].style.display = “block”; } }
+
+// دالة لتحديد جميع أيام الزيارات عند اختيار كل أيام الأسبوع
+function selectAllDays() {
+  var all_days = document.getElementById("all_days");
+  var saturday = document.getElementById("saturday");
+  var sunday = document.getElementById("sunday");
+  var monday = document.getElementById("monday");
+  var tuesday = document.getElementById("tuesday");
+  var wednesday = document.getElementById("wednesday");
+  var thursday = document.getElementById("thursday");
+  if (all_days.checked == true) {
+    saturday.checked = true;
+    sunday.checked = true;
+    monday.checked = true;
+    tuesday.checked = true;
+    wednesday.checked = true;
+    thursday.checked = true;
+  } else {
+    saturday.checked = false;
+    sunday.checked = false;
+    monday.checked = false;
+    tuesday.checked = false;
+    wednesday.checked = false;
+    thursday.checked = false;
+  }
+}
+
+</script> </head> <body> <form method=“post” action=“<?php echo htmlspecialchars($_SERVER[“PHP_SELF”]);?>”> <label for=“user_name”>اسم المستخدم:</label> <input type=“text” id=“user_name” name=“user_name” value=“<?php echo $_SESSION[“email”];?>” readonly> <label for=“customer_name”>اسم العميل:</label> <input type=“text” id=“customer_name” name=“customer_name” maxlength=“40”> <label for=“address”>العنوان:</label> <input type=“text” id=“address” name=“address”> <label for=“phone_number”>رقم التليفو
+
+
+
+
+
+أهلا بك، هذا هو بينغ. يمكنني مساعدتك في إنشاء نموذج لتسجيل بيانات العملاء باستخدام PHP, MySQL, CSS, JS, HTML5. هذا هو الكود الذي أنشأته لك:
+
+<code> <?php // الاتصال بقاعدة البيانات MySQL $servername = “localhost”; $username = “root”; $password = “”; $dbname = “customers”;
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
+
+// التحقق من تسجيل الدخول بالبريد الإلكتروني وكلمة المرور session_start();
+
+if (!isset($_SESSION[‘email’]) || !isset($_SESSION[‘password’])) { header(“Location: login.php”); exit(); }
+
+// جلب اسم المستخدم وتصنيف القطاع من جدول المستخدمين $user_name = $_SESSION[‘email’]; $user_sector = $_SESSION[‘sector’];
+
+// إنشاء جدول العملاء إذا لم يكن موجودًا $sql = “CREATE TABLE IF NOT EXISTS customers ( id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, user_name VARCHAR(30) NOT NULL, customer_name VARCHAR(40) NOT NULL, address VARCHAR(100) NOT NULL, phone_number VARCHAR(11) NOT NULL, port_type VARCHAR(20) NOT NULL, visit_days VARCHAR(50) NOT NULL, customer_category VARCHAR(20) NOT NULL, route VARCHAR(10) NOT NULL )”;
+
+if ($conn->query($sql) === FALSE) { echo "Error creating table: " . $conn->error; }
+
+// إدخال بيانات العميل في جدول العملاء إذا تم الضغط على زر إرسال if (isset($_POST[‘submit’])) { $customer_name = $_POST[‘customer_name’]; $address = $_POST[‘address’]; $phone_number = $_POST[‘phone_number’]; $port_type = $_POST[‘port_type’]; $visit_days = implode(", ", $_POST[‘visit_days’]); $customer_category = $_POST[‘customer_category’]; $route = $_POST[‘route’];
+
+$sql = “INSERT INTO customers (user_name, customer_name, address, phone_number, port_type, visit_days, customer_category, route) VALUES (‘$user_name’, ‘$customer_name’, ‘$address’, ‘$phone_number’, ‘$port_type’, ‘$visit_days’, ‘$customer_category’, ‘$route’)”;
+
+if ($conn->query($sql) === TRUE) { echo “تم إدخال بيانات العميل بنجاح”; } else { echo "Error: " . $sql . “<br>” . $conn->error; } }
+
+// جلب بيانات العملاء من جدول العملاء بناءً على تصنيف القطاع $sql = “SELECT * FROM customers WHERE customer_category = ‘$user_sector’”; $result = $conn->query($sql);
+
+?>
+
+<!DOCTYPE html> <html lang=“ar”> <head> <meta charset=“UTF-8”> <title>نموذج تسجيل بيانات العملاء</title> <style> /* تنسيق النموذج والجدول */ form { margin: 20px; padding: 20px; border: 1px solid black; }
+
+table {
+  margin: 20px;
+  border-collapse: collapse;
+}
+
+th, td {
+  padding: 10px;
+  border: 1px solid black;
+}
+
+</style> <script> // دالة لإظهار حقل route بناءً على اختيار تصنيف العميل function showRoute() { var customer_category = document.getElementById(“customer_category”).value; var route = document.getElementById(“route”); var options = route.options; if (customer_category == “Restaurant”) { options.length = 0; options[0] = new Option(“HO0007”, “HO0007”); options[1] = new Option(“HO0008”, “HO0008”); options[2] = new Option(“HO0009”, “HO0009”); options[3] = new Option(“HO0010”, “HO0010”); options[4] = new Option(“HO0016”, “HO0016”); } else if (customer_category == “Hotel”) { options.length = 0; options[0] = new Option(“HO0005”, “HO0005”); options[1] = new Option(“HO0006”, “HO0006”); options[2] = new Option(“HO0013”, “HO0013”); } else if (customer_category == “Catering”) { options.length = 0; options[0] = new Option(“HO0001”, “HO0001”); options[1] = new Option(“HO0002”, “HO0002”); options[2] = new Option(“HO0003”, “HO0003”); options[3] = new Option(“HO0004”, “HO0004”); options[4] = new Option(“HO0011”, “HO0011”); options[5] = new Option(“HO0012”, “HO0012”); options[6] = new Option(“HO0014”, “HO0014”); } else { options.length = 0; } }
+
+// دالة لتحديد جميع أيام الزيارات إذا تم اختيار كل أيام الأسبوع
+function checkAllDays() {
+  var all_days = document.getElementById("all_days");
+  var visit_days = document.getElementsByName("visit_days[]");
+  if (all_days.checked) {
+    for (var i = 0; i < visit_days.length; i++) {
+      visit_days[i].checked = true;
+    }
+  } else {
+    for (var i = 0; i < visit_days.length; i++) {
+      visit_days[i].checked = false;
+    }
+  }
+}
+
+</script> </head> <body> <h1>نموذج تسجيل بيانات العملاء</h1> <form method=“post” action=“<?php echo $_SERVER[‘PHP_SELF’]; ?>”> <p>اسم المستخدم: <?php echo $user_name; ?></p> <p>أسم العميل: <input type=“text” name=“customer_name” maxlength=“40” required></p> <p>العنوان: <input type=“text” name=“address” required></p> <p>رقم التليفون: <input type=“tel” name=“phone_number” pattern=“[0-9]{11}” required></p> <p>نوع المنفذ: <select name=“port_type” required> <option value=“”>اختر نوع المنفذ</option> <option value=“Ethernet”>Ethernet</option> <option value=“USB”>USB</option> <option value=“Wireless”>Wireless</option> </select></p> <p>ايام الزيارات: <input type=“checkbox” name=“visit_days[]” value=“السبت”> السبت <input type=“checkbox” name=“visit_days[]” value=“الاحد”> الاحد <input type=“checkbox” name=“visit_days[]” value=“الاثنين”> الاثنين <input type=“checkbox” name=“visit_days[]” value=“الثلاثاء”> الثلاثاء <input type=“checkbox” name=“visit_days[]” value=“الاربعاء”> الاربعاء <input type=“checkbox” name=“visit_days[]” value=“الخميس”> الخميس <input type=“checkbox” id=“all_days” value=“كل ايام الاسبوع” onclick=“checkAllDays()”> كل ايام الاسبوع </p> <p>تصنيف العميل: <select id=“customer_category” name=“customer_category” onchange=“showRoute()” required> <option value=“”>اختر تصنيف العميل</option> <option value=“Restaurant”>Restaurant</option> <option value=“Hotel”>Hotel</option> <option value=“Catering”>Catering</option> </select></p> <p>route: <select id=“route” name="route
